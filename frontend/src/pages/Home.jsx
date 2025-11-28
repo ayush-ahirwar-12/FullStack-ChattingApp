e4cae0b019199c7827dabfa339942407a856f4c0
ayush_ahirwar-12 <ayushahirwar04@gmail.com>
2025-11-28 20:18:47 +0530

    final

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar.jsx';
 import { useDispatch, useSelector } from 'react-redux';
 import SideUser from '../components/SideUser';
 import { Outlet } from 'react-router';
