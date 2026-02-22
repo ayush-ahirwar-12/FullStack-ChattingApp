46d1629a81c1cf3a7e391fa94cd0635bf6a72b58
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-02-22 14:48:34 +0530

    ..

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,30 +3,42 @@ import userModel from "../models/user.model.js";
 import cacheClient from "../services/cache.service.js";
 
 const authMiddleware = async (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1] || req.cookies.token;
-  if (!token)
-    return res.status(401).json({ message: "No token, authorization denied." });
-  const isBlacklisted = await cacheClient.get(token);
-  if(isBlacklisted){
-    res.status(422).json({
-      message:"token is blacklisted"
-    })
-  }
-
   try {
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  const user = await userModel.findById(decoded.id);
-  if (!user) {
-    res.status(404).json({ message: "user not found" });  
-  }
-  req.user = user;
-  next();
+      console.log("Cookies:", req.cookies);
+  console.log("Auth header:", req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    const token =
+      (authHeader && authHeader.split(" ")[1]) ||
+      req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "No token, authorization denied.",
+      });
+    }
+
+    const isBlacklisted = await cacheClient.get(token);
+    if (isBlacklisted) {
+      return res.status(422).json({
+        message: "Token is blacklisted",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+    next();
   } catch (error) {
-    return res.status(400).json({
-        message:"token is not valid"
-    })
+    return res.status(401).json({
+      message: "Token is not valid",
+    });
   }
 };
-
 export default authMiddleware;
