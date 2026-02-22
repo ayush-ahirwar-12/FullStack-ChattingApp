c811f3389085cfc407eeff2c01fc64aaef52623d
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-02-22 13:23:29 +0530

    Update axios.jsx

--- frontend/src/axios/axios.jsx
+++ frontend/src/axios/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:5000/api":"/api",
+    baseURL:import.meta.env.MODE==="development"?"https://fullstack-chattingapp-backend.onrender.com":"/api",
     withCredentials:true
 })
 
