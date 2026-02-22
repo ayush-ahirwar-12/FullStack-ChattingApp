483f3510983443bd736c9c301560cfb5c73c2dea
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-02-22 14:09:40 +0530

    kk

--- frontend/src/axios/axios.jsx
+++ frontend/src/axios/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  "https://fullstack-chattingapp-backend.onrender.com";
+  "https://fullstack-chattingapp-backend.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
